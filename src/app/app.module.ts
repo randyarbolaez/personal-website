acc7dbc2f27766d75e0602bd5a82eda78c6e8277
Randy Arbolaez <Randyarbolaez0@gmail.com>
2018-06-21 13:37:38 -0400

    all components & routes made

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { AboutComponent } from './about/about.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+  { path: '', component: LandingPageComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'skills', component: SkillsComponent },
+  { path: 'contact', component: ContactComponent },
+];
 
 @NgModule({
-  declarations: [AppComponent, LandingPageComponent],
-  imports: [BrowserModule],
+  declarations: [
+    AppComponent,
+    LandingPageComponent,
+    AboutComponent,
+    ProjectsComponent,
+    SkillsComponent,
+    ContactComponent,
+  ],
+  imports: [BrowserModule, RouterModule.forRoot(routes), HttpModule],
   providers: [],
   bootstrap: [AppComponent],
 })
