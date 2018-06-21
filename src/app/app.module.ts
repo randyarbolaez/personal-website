aaaa2285e00062c1aa259b9d88c17614f216726b
Randy Arbolaez <Randyarbolaez0@gmail.com>
2018-06-21 13:04:12 -0400

    landing page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LandingPageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
